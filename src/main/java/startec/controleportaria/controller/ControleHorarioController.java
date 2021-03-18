411769469af0a76844d121514dfeb7df82dc39e3
vinycius-cesar <68862312+vinycius-cesar@users.noreply.github.com>
2021-03-18 14:54:04 -0300

    salvando controle hora

--- src/main/java/startec/controleportaria/controller/ControleHorarioController.java
+++ src/main/java/startec/controleportaria/controller/ControleHorarioController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import startec.controleportaria.model.ControleHorario;
 import startec.controleportaria.repository.ControleHorarioRepository;
 
 @Controller
@@ -19,6 +20,17 @@ public class ControleHorarioController {
 		
 		return "controle/controlehorario";
 		
+	}
+	
+	
+	//metodo para salvar as informacoes no BD
+	@RequestMapping(method = RequestMethod.POST, value = "**/salvarControleHorario")
+	public ModelAndView salvar(ControleHorario controleHorario) {
+		
+		controleHorarioRepository.save(controleHorario);
+		ModelAndView modelAndView = new ModelAndView("controle/controlehorario");
+		//Iterable<ControleHorario> pessoaIt = controleHorarioRepository.findAll(); //buscar os dados no banco de dados
+		return modelAndView;
 	}
 
 }
